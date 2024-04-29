cc5c1020570ec8d7356bd8f10f6213fbe55ee69c
Oğuzhan <oguzhankaracorlu@gmail.com>
2024-04-29 23:30:41 +0300

    Create InstastatisticsApplicationTests.java

---
+++ src/test/java/com/oguzhan/karacorlu/instastatistics/InstastatisticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oguzhan.karacorlu.instastatistics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstastatisticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
