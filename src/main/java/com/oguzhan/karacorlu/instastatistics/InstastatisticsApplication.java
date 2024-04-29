e2967ed05bfd31d0e416446668cb56c40f728f64
Oğuzhan <oguzhankaracorlu@gmail.com>
2024-04-29 23:30:30 +0300

    Create InstastatisticsApplication.java

---
+++ src/main/java/com/oguzhan/karacorlu/instastatistics/InstastatisticsApplication.java
@@ -0,0 +1,13 @@
+package com.oguzhan.karacorlu.instastatistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstastatisticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstastatisticsApplication.class, args);
+	}
+
+}
